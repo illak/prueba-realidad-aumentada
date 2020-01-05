8180eb8cc7834ff3c945524ee250ac0f9cd627a4
Illak <i.zapata1989@gmail.com>
2020-01-05 17:27:53 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const hbs = require('hbs');
+
+
+const port = process.env.PORT || 3000;
+
+
+app.use( express.static( __dirname + '/public' ) );
+
+
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${ port }`);
+})
