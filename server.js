be8699d3630b7a806ed04c8a1169f8120dc86eaa
Illak <i.zapata1989@gmail.com>
2020-01-05 17:31:36 -0300

    fixed error

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const app = express()
 
-const hbs = require('hbs');
-
-
 const port = process.env.PORT || 3000;
 
-
 app.use( express.static( __dirname + '/public' ) );
-
 
 app.listen(port, () => {
   console.log(`Escuchando peticiones en el puerto ${ port }`);
